d602d5fa08a4b8b557d1d76d8df882d8f64c840a
hfyzalswfy <hfyzalswfy@gmail.com>
2025-02-19 15:04:03 +0300

    Add files via upload

---
+++ hafeez/Form1.Designer.cs
@@ -0,0 +1,352 @@
+﻿namespace hafeez
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtemail = new System.Windows.Forms.TextBox();
+            this.txtpassword = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.rdomale = new System.Windows.Forms.RadioButton();
+            this.rdofemale = new System.Windows.Forms.RadioButton();
+            this.rdothers = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.brdate = new System.Windows.Forms.DateTimePicker();
+            this.lbloutput = new System.Windows.Forms.Label();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.optionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cbxcountre = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lbldata = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 34);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "UserName:";
+            // 
+            // txtemail
+            // 
+            this.txtemail.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtemail.Location = new System.Drawing.Point(189, 98);
+            this.txtemail.Name = "txtemail";
+            this.txtemail.Size = new System.Drawing.Size(266, 33);
+            this.txtemail.TabIndex = 2;
+            this.txtemail.TextChanged += new System.EventHandler(this.txtemail_TextChanged);
+            // 
+            // txtpassword
+            // 
+            this.txtpassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtpassword.Location = new System.Drawing.Point(188, 138);
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.Size = new System.Drawing.Size(266, 33);
+            this.txtpassword.TabIndex = 3;
+            this.txtpassword.UseSystemPasswordChar = true;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Pink;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(473, 567);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(198, 69);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "LogIn";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // rdomale
+            // 
+            this.rdomale.AutoSize = true;
+            this.rdomale.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.rdomale.Checked = true;
+            this.rdomale.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.rdomale.Location = new System.Drawing.Point(9, 8);
+            this.rdomale.Name = "rdomale";
+            this.rdomale.Size = new System.Drawing.Size(103, 38);
+            this.rdomale.TabIndex = 6;
+            this.rdomale.TabStop = true;
+            this.rdomale.Text = "Male";
+            this.rdomale.UseVisualStyleBackColor = false;
+            // 
+            // rdofemale
+            // 
+            this.rdofemale.AutoSize = true;
+            this.rdofemale.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.rdofemale.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.rdofemale.Location = new System.Drawing.Point(118, 8);
+            this.rdofemale.Name = "rdofemale";
+            this.rdofemale.Size = new System.Drawing.Size(138, 38);
+            this.rdofemale.TabIndex = 7;
+            this.rdofemale.Text = "Female";
+            this.rdofemale.UseVisualStyleBackColor = false;
+            // 
+            // rdothers
+            // 
+            this.rdothers.AutoSize = true;
+            this.rdothers.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.rdothers.Location = new System.Drawing.Point(263, 8);
+            this.rdothers.Name = "rdothers";
+            this.rdothers.Size = new System.Drawing.Size(131, 38);
+            this.rdothers.TabIndex = 8;
+            this.rdothers.Text = "Others";
+            this.rdothers.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(85, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 34);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Email:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(160, 34);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Password:";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.panel1.Controls.Add(this.rdofemale);
+            this.panel1.Controls.Add(this.rdomale);
+            this.panel1.Controls.Add(this.rdothers);
+            this.panel1.Location = new System.Drawing.Point(161, 297);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(390, 55);
+            this.panel1.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 305);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(128, 34);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Gender:";
+            // 
+            // brdate
+            // 
+            this.brdate.Location = new System.Drawing.Point(188, 234);
+            this.brdate.Name = "brdate";
+            this.brdate.Size = new System.Drawing.Size(267, 40);
+            this.brdate.TabIndex = 14;
+            // 
+            // lbloutput
+            // 
+            this.lbloutput.AutoSize = true;
+            this.lbloutput.BackColor = System.Drawing.Color.White;
+            this.lbloutput.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.lbloutput.Location = new System.Drawing.Point(26, 472);
+            this.lbloutput.Name = "lbloutput";
+            this.lbloutput.Size = new System.Drawing.Size(0, 34);
+            this.lbloutput.TabIndex = 15;
+            this.lbloutput.Click += new System.EventHandler(this.lbloutput_Click);
+            // 
+            // txtname
+            // 
+            this.txtname.BackColor = System.Drawing.Color.White;
+            this.txtname.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtname.Location = new System.Drawing.Point(188, 59);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(267, 33);
+            this.txtname.TabIndex = 16;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.White;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(700, 45);
+            this.menuStrip1.TabIndex = 17;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.optionToolStripMenuItem});
+            this.toolStripMenuItem1.Font = new System.Drawing.Font("Segoe UI", 16F);
+            this.toolStripMenuItem1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(173, 41);
+            this.toolStripMenuItem1.Text = "Select Color";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // optionToolStripMenuItem
+            // 
+            this.optionToolStripMenuItem.Name = "optionToolStripMenuItem";
+            this.optionToolStripMenuItem.Size = new System.Drawing.Size(259, 42);
+            this.optionToolStripMenuItem.Text = "Open Dialog";
+            this.optionToolStripMenuItem.Click += new System.EventHandler(this.optionToolStripMenuItem_Click);
+            // 
+            // colorDialog1
+            // 
+            this.colorDialog1.AnyColor = true;
+            this.colorDialog1.FullOpen = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(49, 171);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(138, 34);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Countre:";
+            // 
+            // cbxcountre
+            // 
+            this.cbxcountre.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.cbxcountre.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.cbxcountre.FormattingEnabled = true;
+            this.cbxcountre.Items.AddRange(new object[] {
+            "YEMEN",
+            "USA",
+            "EGYPT",
+            "CSA",
+            "OMAN",
+            "ERAQ",
+            "ORDON",
+            "OTHER",
+            "YABAN",
+            "UONAN"});
+            this.cbxcountre.Location = new System.Drawing.Point(188, 177);
+            this.cbxcountre.Name = "cbxcountre";
+            this.cbxcountre.Size = new System.Drawing.Size(267, 41);
+            this.cbxcountre.TabIndex = 19;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(26, 234);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(161, 34);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "BirthDate:";
+            // 
+            // lbldata
+            // 
+            this.lbldata.AutoSize = true;
+            this.lbldata.BackColor = System.Drawing.Color.White;
+            this.lbldata.Font = new System.Drawing.Font("Tahoma", 19.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbldata.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lbldata.Location = new System.Drawing.Point(14, 431);
+            this.lbldata.Name = "lbldata";
+            this.lbldata.Size = new System.Drawing.Size(0, 41);
+            this.lbldata.TabIndex = 21;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 33F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(700, 676);
+            this.Controls.Add(this.lbldata);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cbxcountre);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.lbloutput);
+            this.Controls.Add(this.brdate);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtpassword);
+            this.Controls.Add(this.txtemail);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Tahoma", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Navy;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.Name = "Form1";
+            this.Text = "HOME";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtemail;
+        private System.Windows.Forms.TextBox txtpassword;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton rdomale;
+        private System.Windows.Forms.RadioButton rdofemale;
+        private System.Windows.Forms.RadioButton rdothers;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker brdate;
+        private System.Windows.Forms.Label lbloutput;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem optionToolStripMenuItem;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cbxcountre;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lbldata;
+    }
+}
+
---
+++ hafeez/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace hafeez
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            
+
+        }
+
+        private void lbloutput_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void AddInformation()
+        {
+            if (txtname.Text == "")
+            {
+                lbloutput.Text = "";
+                MessageBox.Show("please enter user name","field requere",MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                txtname.Focus();
+                
+                return;
+            }
+            else if (txtemail.Text == "")
+            {
+                MessageBox.Show("please enter your email", "field requere", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                txtemail.Focus();
+                return;
+            }
+            else if (txtpassword.Text == "")
+            {
+                lbloutput.Text = "";
+                MessageBox.Show("please enter user password", "field requere", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                txtpassword.Focus();
+                return;
+            }
+            else
+            {
+                string chick = "male";
+                if (rdofemale.Checked)
+                {
+                    chick = "female";
+                }
+                else if (rdothers.Checked)
+                {
+                    chick = "others";
+                }
+                else
+                {
+                    chick = "male";
+                }
+
+                DateTime dt = brdate.Value;
+                lbldata.Text = "YOUR DATA IS:";
+                lbloutput.Text = "name: " + txtname.Text
+                    + "\t\n" + "email: " + txtemail.Text
+                    + "\t\n" + "password: " + txtpassword.Text
+                    + "\n gender: " + chick
+                    + "\n brith date: " + dt.ToString()
+                    + "\n countre: " + cbxcountre.Text;
+            }
+        }
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            
+            string email = txtemail.Text;
+            Regex regex = new Regex(@"^\w+([-_.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$");
+
+            if (regex.IsMatch(email))
+            {
+
+                AddInformation();
+
+
+
+             
+
+            }
+            else
+            {
+                lbloutput.Text = "";
+                MessageBox.Show("please enter correct Email", "field requere", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtemail.Focus();
+                
+            }
+        }
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripComboBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void optionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                this.BackColor = colorDialog1.Color;
+
+            }
+        }
+
+        private void txtemail_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
